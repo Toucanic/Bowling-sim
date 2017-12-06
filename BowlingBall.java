732a8f844a8302df71aac6946a42c12fcf68f7a7
Toucanic <31905870+Toucanic@users.noreply.github.com>
2017-12-06 09:23:48 -0800

    Add files via upload

---
+++ BowlingBall.java
@@ -0,0 +1,13 @@
+package physical_Items;
+
+public class BowlingBall {
+	
+	int radius = 0;
+	int mass = 0;
+	int materialFriction = 0;
+	
+	public BowlingBall(int radius, int weight, int material) {
+		
+	}
+
+}
